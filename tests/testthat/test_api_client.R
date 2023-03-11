f8f913e7c4e2cc6f00a94e42687f271233eef94d
Hossein Falaki <hossein@databricks.com>
2023-03-11 10:39:10 -0800

    Adding some more dummy tests (#2)
    * Adding some more dummy tess
    
    * installing devtools
    
    * Update R/api_client.R

--- tests/testthat/test_api_client.R
+++ tests/testthat/test_api_client.R
@@ -1,3 +1,17 @@
+
+test_that("we capture user agent properly", {
+  expect_equal(databricks:::.productInfo, R.Version()$version.string)
+})
+
+test_that("loads configuration file", {
+  expect_true(
+    databricks:::.DatabricksClient(config_file = "./data/azurecfg")$is_azure())
+  expect_true(
+    databricks:::.DatabricksClient(config_file = "./data/awscfg")$is_aws())
+  expect_true(
+    databricks:::.DatabricksClient(config_file = "./data/gcpcfg")$is_gcp())
+})
+
 test_that("always fails", {
 		databricks::configure(host="https://demo", token="foo")
 		expect_error({
